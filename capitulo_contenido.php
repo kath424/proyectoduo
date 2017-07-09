85e535f552943216b63ed48f710e5a828c382203
kath424 <acostak424@gmail.com>
2017-07-09 04:38:13 -0500

    corrige botones de navegacion aniade restriccion para tomar pruba solo una vez

--- capitulo_contenido.php
+++ capitulo_contenido.php
@@ -27,22 +27,43 @@
 	<img src="img/cursos/logica/modulo1.<?php echo $_GET['paso'] ?>.PNG" 
 		style="width:100%;" />
 
-	<div  style="width: 100%"> 
-		<?php if( intval($_GET['paso']) > 1) { ?>
-		<a class="btn btn-blanco izquierda" href="capitulo_contenido.php?id=<?php echo $_GET['id']; ?>&paso=<?php echo intval($_GET['paso'])-1; ?>"> previous </a>
-		<?php } ?>
-		<a class="btn btn-verde derecha" href="capitulo_contenido.php?id=<?php echo $_GET['id']; ?>&paso=<?php echo intval($_GET['paso'])+1; ?>"> siguiente </a>
-	</div>
 </section>
+<div  style="width: 100%"> 
+	<?php if( intval($_GET['paso']) > 1) { ?>
+	<a class="btn btn-blanco izquierda" href="capitulo_contenido.php?id=<?php echo $_GET['id']; ?>&paso=<?php echo intval($_GET['paso'])-1; ?>"> previous </a>
+	<?php } ?>
+	<a class="btn btn-verde derecha" href="capitulo_contenido.php?id=<?php echo $_GET['id']; ?>&paso=<?php echo intval($_GET['paso'])+1; ?>"> siguiente </a>
+</div>
 
 <?php } else { ?>
+
+<?php 
+	$respuestas_query = "select * from estudiante_respuestas er"
+		." left join preguntas p"
+		." on er.preguntas_id = p.id"
+		." where "
+		." usuarios_id = ". $_SESSION['user_id']
+		." and"
+		." capitulos_id = ". $_GET['id'];
+
+	$resultado = $mysqli->query($respuestas_query);
+	if($resultado){
+		$respuestas = $resultado;
+	}
+
+ ?>
 
 <section id="banner">
 	<div style="text-align: center">
 		<h1 > Curso Finalizado </h1>
 	</div>
 	<div style="text-align: center">
+		
+		<?php if(!$respuestas->num_rows > 0){  ?>
 		<a class="btn btn-verde btn-grande " href="evaluacion.php?id=<?php echo $_GET['id'] ?>"> Realizar Evaluacion Ahora</a>
+		<?php }else{ ?>
+		<div class="btn btn-blanco btn-grande"> Prueba Realizada </div>
+		<?php } ?>
 	</div>
 </section>
 
