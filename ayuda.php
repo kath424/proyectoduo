92c2fe3777e858b5387c744d5a0ccec33c6874d6
kath424 <acostak424@gmail.com>
2017-08-08 21:42:55 -0500

    seccion de ayuda

--- ayuda.php
+++ ayuda.php
@@ -8,9 +8,9 @@ require('barra_de_navegacion.php');
 
 <div class="row">
     <div class="col-sm-12">
-        <h1 class="text-center">AYUDA</h1>
+        <img class="img-responsive" src="img/instrucciones/GUIA PARA ADMINISTRADOR.jpg">
     </div>
     <div class="col-sm-12">
-        <p>Contenido Aqui</p>
+        <img class="img-responsive" src="img/instrucciones/GUIA PARA ESTUDIANTES.jpg">
     </div>
 </div>
