5e24c7ea0fb042f3b398acc78a67407cca639aad
kath424 <acostak424@gmail.com>
2017-08-13 17:45:49 -0500

    muestra caracateres utf correctamente

--- conneccion.php
+++ conneccion.php
@@ -3,5 +3,7 @@ $mysqli = new mysqli('localhost', 'root', '', 'projecto_duo_db');
 
 if ($mysqli->connect_errno) {
     echo "fallo al conectarse a MySQL: " . mysqli_connect_error();
+}else{
+    $mysqli->query("SET NAMES 'utf8'");
 }
 
